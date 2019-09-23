03792d2f1b5af9ec80d625de5e811bbca4f442d1
Omkar Chorghe <omkarchorghe22@gmail.com>
2019-09-23 16:06:30 +0530

    Quiz_Detail

--- Quiz/views.py
+++ Quiz/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from django.shortcuts import get_object_or_404,render
+from django.core.exceptions import PermissionDenied
+
 from Quiz.models import *
 
 # Create your views here.
@@ -30,3 +32,18 @@ class View_Quizlist_by_Category(ListView):
     def get_queryset(self):
         queryset = super(View_Quizlist_by_Category, self).get_queryset()
         return queryset.filter(category = self.category, draft = False)
+    
+class Quiz_Detail_View(DetailView):
+    model = Quiz
+    slug_field = 'url'
+    template_name = 'Quiz/quiz_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        
+        self.object = self.get_object()
+
+        if self.object.draft and not request.user.has_perm('quiz.change_quiz'):
+            return PermissionDenied
+        
+        context = self.get_context_data(object = self.object)
+        return render(request,self.template_name,{'quiz':self.object})
