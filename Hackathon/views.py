84da40fe763a5a5eb7546085b0288b2864b9e1f4
Omkar <omkarchorghe22@gmail.com>
2020-01-18 14:59:31 +0530

    modified quiz category listing

--- Hackathon/views.py
+++ Hackathon/views.py
@@ -2,3 +2,5 @@ import os
 from django.shortcuts import render
 from django.views import View
 
+def hackathon_home(request, *args, **kwargs):
+    return render(request, "Hackathon/hackathon_home.html", {})
