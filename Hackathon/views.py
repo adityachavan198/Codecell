533c29e3e5193f0dcdbf43998cbe59b0c4485924
sagarambilpure <37987124+sagarambilpure@users.noreply.github.com>
2020-01-18 19:09:20 +0530

    bhendi tu dekhle ab

--- Hackathon/views.py
+++ Hackathon/views.py
@@ -1,6 +1,16 @@
 import os
 from django.shortcuts import render
 from django.views import View
+from Hackathon.models import *
 
 def hackathon_home(request, *args, **kwargs):
     return render(request, "Hackathon/hackathon_home.html", {})
+
+def hackathon_register(request):
+    problems_db = problems.objects.all()
+    problems_var=dict()
+    for i in problems_db:
+        problems_var[i.id]={"title":i.title,"statement":i.statement,"category":i.Category}
+    print("\n\n\n\nnsgasfg\n\n\n\n",problems_var)
+
+    return render(request,"Hackathon/hackathon_register.html",{'problems':problems_var})
