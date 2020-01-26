cc739e91d28387716e4da8e99ffd65c48f4e9fbd
Nitish <nitishtalekar.nt503@gmail.com>
2020-01-26 19:22:32 +0530

    HOME

--- Codecell/settings.py
+++ Codecell/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a6ri_w$)txz3t@l!*k%a2w8r04=27$n7b0r!w!9mzcb=hauhip'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0','192.168.0.2']
 
 
 # Application definition
