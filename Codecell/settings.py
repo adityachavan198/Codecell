c0f4e7a8794551eb0fb035a707accdb275a33f12
Omkar <omkarchorghe22@gmail.com>
2020-01-26 10:51:21 +0530

    server files

--- Codecell/settings.py
+++ Codecell/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a6ri_w$)txz3t@l!*k%a2w8r04=27$n7b0r!w!9mzcb=hauhip'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0','omkarc.pythonanywhere.com',]
 
 
 # Application definition
@@ -122,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
